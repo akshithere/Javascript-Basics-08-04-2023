17986119f7e377953454a056b7cb2483fd82e61a
akshithere <130065975+akshithere@users.noreply.github.com>
2023-04-09 01:18:33 +0530

    update

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ Reference Datatypes: Arrays and Objects
 */
 // Operators 
 //Assignment operators
-  var a = 16;
-  var b = 17;
+  // var a = 16;
+  // var b = 17;
 //   var c = b;
 // //  console.log(c);
 // c /= 4;
@@ -63,24 +63,34 @@ Reference Datatypes: Arrays and Objects
 // console.log(false || true)
 // console.log(false && true)
 
-function avg(a, b) {
+// function avg(a, b) {
 
-return (a+b)/2;
+// return (a+b)/2;
     
-}
-a1 = avg(12, 56);
-a2 = avg(32, 45);
-console.log(a1, a2);
+// }
+// a1 = avg(12, 56);
+// a2 = avg(32, 45);
+// console.log(a1, a2);
 
-if(a > b) {
+// if(a > b) {
 
-console.log("Yes a is greater than b");
+// console.log("Yes a is greater than b");
 
-}
+// }
 
-else {
+// else {
 
-console.log("a is smaller than b");
+// console.log("a is smaller than b");
 
+
+// }
+
+//Loops- 1. For Loop
+var array = [1,2,3,4,5,6,7];
+// console.log(array);
+
+for(var i=0; i<array.length; i++)
+{
+console.log(array[i]);
 
 }
