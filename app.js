44f5e8a37f8168a86c77873600e9ea88563f002f
akshithere <130065975+akshithere@users.noreply.github.com>
2023-04-08 22:36:41 +0530

    Index html and javascript application file

---
+++ app.js
@@ -0,0 +1,86 @@
+// // var number1 = 12;
+// // var number2 = 56;
+// // console.log(number1 + number2) 
+// // Datatypes
+// //3. Objects
+// // var marks = {
+// // akshit: 99,
+// // rishabh: 33,
+// // dhurv: 1
+// // }
+
+
+
+// // console.log(marks);
+// // var a = true;
+// // var b = false;
+// // console.log(a)
+// // var und = undefined;
+// // var und1;
+// // console.log(und, und1);
+// // 3. objects
+//  var a = {
+// Raju: 832,
+// hjuqw: 9e32
+
+
+
+//  }   
+
+// //4. undefined
+// var und2;
+// // console.log(und2)
+// var thiss = null;
+// // console.log(thiss);
+//  //5. Arrays
+//  var arr = [1,2,"Sex",3,4,41];
+/* primitive datatypes and Reference datatype
+Primitive: Numbers, Strings, Boolean, Symbols, undefined and null
+
+
+Reference Datatypes: Arrays and Objects
+*/
+// Operators 
+//Assignment operators
+  var a = 16;
+  var b = 17;
+//   var c = b;
+// //  console.log(c);
+// c /= 4;
+// console.log(c);
+// //    Arithmetic Operators
+//  var d;
+//  console.log("The value of d is actually sum of a and b that is", a+b);
+//  d = a + b;
+//  // Comparison Operators
+//  console.log(a == d);
+//  console.log(a>=d);
+//  console.log(a<=d);
+
+ 
+//Logical operators
+
+// console.log(false || true)
+// console.log(false && true)
+
+function avg(a, b) {
+
+return (a+b)/2;
+    
+}
+a1 = avg(12, 56);
+a2 = avg(32, 45);
+console.log(a1, a2);
+
+if(a > b) {
+
+console.log("Yes a is greater than b");
+
+}
+
+else {
+
+console.log("a is smaller than b");
+
+
+}
